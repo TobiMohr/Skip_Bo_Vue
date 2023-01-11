3cbee4adb126d2d886d74bdabcf5bac24010d9ba
SponsoredByPuma <92574150+SponsoredByPuma@users.noreply.github.com>
2023-01-11 12:12:48 +0100

    added less loader

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,43 @@
 <template>
-    <q-page>
-      <GameBoard></GameBoard>
-    </q-page>
-  </template>
-  
-  <style>
-  </style>
-  
-  <script>
-  import GameBoard from '@/components/GameBoard.vue';
-  export default {
-    name: "Skip_Bo",
-    components: {GameBoard},
-  }
-  </script>
-  
+  <q-page>
+    <GameBoard></GameBoard>
+  </q-page>
+</template>
+<script>
+import GameBoard from '@/components/GameBoard.vue';
+import $ from "jquery";
+export default {
+  name: "Skip_Bo",
+  components: { GameBoard },
+
+  data() {
+    return {
+      backendOnline: false,
+    }
+  },
+  mounted() {
+    $.ajax({
+      url: 'http://localhost:9000/status'
+    }).done(() => {
+      this.backendOnline = true;
+    });
+  },
+}
+</script>
+<style lang="less">
+@x-small: 575px; // >=
+@small: 767px; // >=
+@medium: 991px; // >=
+@large: 1199px; // >=
+@xl: 1399px; // >=
+@xxl: 1400px; // <=
+/*
+Breakpoint	Class	Dimensions
+X-Small	None	< 576px
+Small	sm	≥ 576px
+Medium	md	≥ 768px
+Large	lg	≥ 992px
+Extra large	xl	≥ 1200px
+Extra extra large	xxl	≥ 1400px
+ */
+</style>
