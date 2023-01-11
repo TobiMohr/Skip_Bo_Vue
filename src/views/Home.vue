bd7c7d2cbc5abdadbe3ac0881a5e60e3592d5de3
SponsoredByPuma <92574150+SponsoredByPuma@users.noreply.github.com>
2023-01-11 14:43:57 +0100

    add the medias for the different devices

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,12 +25,18 @@ export default {
 }
 </script>
 <style lang="less">
+// responsive
+@card_size: 2vw;
+@gameBoard_width: calc(@card_size * 6);
+@gameBoard_height: calc(@card_size * 3);
+
 @x-small: 575px; // >=
 @small: 767px; // >=
 @medium: 991px; // >=
 @large: 1199px; // >=
 @xl: 1399px; // >=
 @xxl: 1400px; // <=
+
 /*
 Breakpoint	Class	Dimensions
 X-Small	None	< 576px
@@ -40,4 +46,68 @@ Large	lg	≥ 992px
 Extra large	xl	≥ 1200px
 Extra extra large	xxl	≥ 1400px
  */
+// XS
+@media(max-width: @x-small) and (orientation: landscape) {
+  @card_size: 1vw;
+  @gameBoard_width: calc(@card_size * 6);
+  @gameBoard_height: calc(@card_size * 3);
+
+  // hier müssen jetzt noch divs von der Gamecomponent gestylet werden 
+}
+
+@media(max-width: @x-small) and (orientation: portrait) {
+  @card_size: 2.25vh;
+}
+
+// S
+@media(max-width: (@small - 1)) and (min-width: (@x-small + 1)) and (orientation: landscape) {
+  @card_size: 1vw;
+  @gameBoard_width: calc(@card_size * 6);
+  @gameBoard_height: calc(@card_size * 3);
+}
+
+@media (max-width: (@small - 1)) and (min-width: (@x-small + 1)) and (orientation: portrait) {
+  @card_size: 2.25vh;
+  @gameBoard_width: calc(@card_size * 6);
+  @gameBoard_height: calc(@card_size * 3);
+}
+
+// M
+@media (max-width: (@medium - 1)) and (min-width: (@small + 1)) and (orientation: landscape) {
+  @card_size: 1vw;
+  @gameBoard_width: calc(@card_size * 6);
+  @gameBoard_height: calc(@card_size * 3);
+}
+
+@media (max-width: (@medium - 1)) and (min-width: (@small + 1)) and (orientation: portrait) {
+  @card_size: 2.75vh;
+  @gameBoard_width: calc(@card_size * 6);
+  @gameBoard_height: calc(@card_size * 3);
+}
+
+// L
+@media (max-width: (@large - 1)) and (min-width: (@medium + 1)) and (orientation: landscape) {
+  @card_size: 2vw;
+  @gameBoard_width: calc(@card_size * 6);
+  @gameBoard_height: calc(@card_size * 3);
+}
+
+@media(max-width: (@large - 1)) and (min-width: (@medium + 1)) and (orientation: portrait) {
+  @card_size: 2.25vh;
+  @gameBoard_width: calc(@card_size * 6);
+  @gameBoard_height: calc(@card_size * 3);
+}
+
+// XL
+@media(min-width: @large) and (orientation: landscape) {
+  @card_size: 2vw;
+  @gameBoard_width: calc(@card_size * 6);
+  @gameBoard_height: calc(@card_size * 3);
+}
+
+@media(min-width: @large) and (orientation: portrait) {
+  @card_size: 2.25vh;
+  @gameBoard_width: calc(@card_size * 6);
+  @gameBoard_height: calc(@card_size * 3);
+}
 </style>
